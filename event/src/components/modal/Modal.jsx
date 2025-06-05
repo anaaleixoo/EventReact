418e332990cef09df031b745b4813b21f7912abf
Ana Aleixo <ana.c.silva403@aluno.senai.br>
2025-06-05 16:44:52 -0300

    atualizando tela evento

--- event/src/components/modal/Modal.jsx
+++ event/src/components/modal/Modal.jsx
@@ -1,47 +1,85 @@
-import react from 'react';
+import { useEffect, useState } from "react"
+import imgDeletar from"../../assets/img/Excluir.svg";
+
+
 const Modal = (props) => {
-    return (<>
-        <div className="model-overlay" onClick={props.fecharModal}>
-        </div>
-        <div className="model">
-            <h1>{props.titulo}</h1>
-            <div className="modal_conteudo">
-                {props.tipoModel === "descricaoEvento" ? (
-                    <p>{props.descricao}</p>
-                ) : ( 
-                    <>
-                       {comentarios.map((item) => (
-                        <div key={item.idcomentarioEvento}> 
-                        <strong>{item.usuario.nomeUsuario}
-                        </strong>
-                        <img src={imgDeletar}
-                        alt="Deletar" /> 
-                        <p>{item.descricao}</p>
-                        <hr />
-                        </div>
-                       )
-                       )}
-                    </>
-                )} 
-
-                <div>
-                    <input type="text"
-                    placeholder="Escreva seu comentario..
-                    " />
-                    <button>
-                        Cadastrar
-                    </button>
-                </div>
-
-            </div>
-        </div>
+  const [comentarios, setComentarios] = useState([])
+  const [novoComentario, setNovoComentario] =
+async function listarComentarios() {
+  try {
+    await api.get(`comentariosEventos/ListarSomenteExibe?id=${props.idEvento}`);
+    setComentarios(resposta.data)
 
 
+  } catch (error) {
+    console.log(error);
+  }
+  
+}
+useEffect(() => {
+  listarComentarios
+}, [])
 
-    </>
-    )
 
+async function cadastrarComentario() {
+  try {
+    await api.post("ComentariosEventos",(
+      idUsuario = usuarioid,
+      idEvento = props.idEvento,
+      descricao = comentarios
+    ));
+  } catch (error) {
+    
+  }
 
+  async function deletarcomentario(idcomentario) {
+    try{
+        await api.delete(`ComentarioEventos/${idComentario}`);
+    } catch (error) {
+        console.log(error)
+    }
+    
+  }
+  
 }
 
-export default Modal 
+  return (<>
+    <div className="model-overlay" onClick={props.fecharModal}></div>
+    <div className="model">
+      <h1>{props.titulo}</h1>
+    <div className="model_conteudo">
+      {props.TipoModel === "descricaoEvento" ? (
+        <p>{props.descricao}</p>
+      ) : (
+        <>
+        {comentarios.map((item) => (
+          <div key={item.idComentarioEvento}>
+            <strong> {item.usuario.nomeUsuario}
+            </strong>
+            <img src={imgDeletar} alt="Deletar"/>
+            onClick={() => deletarcomentario(item.idcomentario)}
+            <p>{item.descricao}</p>
+            <hr />
+          </div>
+        )
+        )}
+        <div>
+          <input type="text"
+          placeholder="escreva seu Comentario..."
+          value={novoComentario}
+          onChange={(e)=> setNovoComentario(e.target.value)}
+           />
+           <button onClick={() => cadastrarComentario(novoComentario)}>
+            Cadastrar
+           </button>
+        </div>
+        </>
+      )}
+    </div>
+    </div>
+  
+  </>
+  )
+}
+
+export default Modal
--- event/src/pages/listagemEventos/ListagemEventos.jsx
+++ event/src/pages/listagemEventos/ListagemEventos.jsx
@@ -1,27 +1,110 @@
 import { useEffect, useState } from "react";
 import api from "../../services/Services";
-
+import Modal from "../../components/modal/Modal.jsx"
 import "./ListagemEventos.css"
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import Comentario from "../../assets/img/Comentario.svg"
 import Toggle from "../../components/toggle/Toggle";
 import Descricao from "../../assets/img/Descricao.svg"
-import Modal from "../../components/modal/Modal";
+import Swal from "sweetalert2";
 
 const ListagemEventos = (props) => {
     const [listaEventos, setListaEventos] = useState([]);
-
-    async function listarEventos() {
+    const [tipoModal, setTipoModel] = useState("");
+    const [dadosModal, setDadosModel] = useState([]);
+    const [modalAberto, setModalAberto] = useState(false);
+    const {filtroData, setFiltroData} = useState(["todos"])
+}
+    const [usuarioid, setUsuarioid] = useState ("")
+    async function listarEventos() {  
         try {
             const resposta = await api.get("Eventos");
+            const todosOsEventos = resposta.data;
+
+            const respostaPresenca = await api.get("PresencaEventos/ListarMinha/"+usuarioD)
+            const minhasPresencas = respostaPresenca.data;
+            const eventosComPresencas = todosOsEventos.map((atualEvento) => {
+                const presenca = minhasPresencas.find(p => p.idEvento
+                    === atualEvento.idEvento);
+
+                    return(
+                        atualEvento,
+                        possuiPresenca = presenca?.situacao === true,
+                        idPresenca = presenca?.idPresencaEvento || null
+                    )
+
+            })
+        
 
             setListaEventos(resposta.data);
+
         } catch (error) {
-            console.log(error);
+            console.log(`informacoes de todos os eventos`);
+            console.log()
+            console.log(`informacoes de eventos com presenca`);
+            console.log()
+            console.log(`informacoes de todos os eventos com presenca`);
+            console.log(eventosComPresencas);
         }
-    }
-    useEffect(() => {
+    
+
+
+    useEffect(() => 
+        function abrirModal(tipo, dados){
+         setModalAberto(true)   
+         tipoModal(tipo)
+         dadosModal(dados)
+
+        })
+          useEffect(() => {
+        function fecharModal(tipo, dados){
+         setModalAberto(false);   
+         tipoModal({});
+         dadosModal("");
+
+        }
+
+        async function manipularPresenca(idEvento, presenca, idPresenca) {
+            try {
+                if(presenca && idPresenca !=""){
+                    await api.put(`presencaEventos/${idPresenca}`),
+                    {situacao: false};
+                    Swal.fire(`removido!`, `Sua presenca foi removida.`, `Sucess`);
+
+                }else if (idPresenca != ""){
+                     await api.put(`presencaEventos/${idPresenca}`),
+                    {situacao: true};
+                    Swal.fire(`Confirmado!`, `Sua presenca foi comfirmada.`, `Sucess`);
+
+
+                }else{
+                await api.post("presencaEventos",{situacao: true, idUsuario: usuarioid, idEvento: idEvento});
+                 Swal.fire(`Confirmado!`, `Sua presenca foi comfirmada.`, `Sucess`);
+
+                }
+                
+            } catch (error) {
+                
+            }
+            function filtrarEventos(){
+                const hoje = new Date();
+
+                return listaEventos.filter(evento => {
+                    const dataEvento = new Date(evento.dataEvento);
+                    if (filtroData.includes("todos")) return true;
+                    if (filtroData.includes("futuros") && dataEvento > hoje)
+                        return true;
+                    if (filtroData.includes("passados") && dataEvento > hoje)
+                        return true;
+
+                    return false;
+                });
+
+            }
+            
+        }
+
         listarEventos();
     }, [])
 
@@ -37,7 +120,11 @@ const ListagemEventos = (props) => {
                         <h1>Eventos</h1>
                         <hr />
                     </div>
-
+                         <select onChange= {(e) => setFiltroData([e.target.value])}>
+                        <option value="todos" selected>Todos os eventos</option> 
+                        <option value="Futuros">Somente futuros</option>
+                        <option value="passados">Somente passados</option>
+                    </select>
                     <div className="listagem_eventos">
                         <select name="eventos">
                             <option value="" disabled selected>Todos os Eventos</option>
@@ -56,19 +143,22 @@ const ListagemEventos = (props) => {
                                 <th>Participar</th>
                             </tr>
                             
+                            
 
                             <tr className="list_presenca">
-                                <td>Festa Buffet</td>
-                                <td>04/06/2025</td>
-                                <td>Trabalho</td>
+                                <td>buffet do buffet</td>
+                                <td>07/06/2025</td>
+                                <td>trabalho</td>
                                 <td>
-                                    <button className="icon">
+                                    <button className="icon" onClick={() => abrirModal("descricaoEvento", 
+                                        item.Descricao)}>
                                         <img src={Descricao} alt="" />
                                     </button>
                                 </td>
 
                                     <td>
-                                    <button className="icon">
+                                    <button className="icon" onClick={() => abrirModal("comentarios", 
+                                        {idEvento: item.idEvento})}>
                                         <img src={Comentario} alt="" />
                                     </button>
                                 </td>
@@ -81,12 +171,21 @@ const ListagemEventos = (props) => {
                     </div>
                 </section>
             </main>
-            <Footer
-                visibilidade="none"
+            <Footer visibilidade="none"
             />
-            <Modal/>
+            {modalAberto && (
+            <Modal
+             titulo = {tipoModel == "descricaoEvento" ? "Descrição do evento" : "Comentarios"}
+             tipoModal = {tipoModal}
+             idEvento = {dadosModal.idEvento}
+             descricao = {dadosModal.descricao}
+             fecharModal = {fecharModal}
+             />
+            )}
         </>
+            
+            
     )
-}
+    }
 
 export default ListagemEventos;
